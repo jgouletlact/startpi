85e1650ee3005ffefa9dba5f6b8fc899d6cd4099
Jason Goulet-Lipman <jason.gouletlipman@gmail.com>
2023-11-03 14:33:21 -0400

    added myoled changes

--- src/myoled.cpp
+++ src/myoled.cpp
@@ -10,16 +10,16 @@ myOled::myOled(int16_t oledwidth, int16_t oledheight) :myOled_graphics(oledwidth
 	bufferHeight = _OLED_HEIGHT;
 }
 
-void myOled::OLEDbegin( uint16_t I2C_speed , uint8_t I2c_address)
+void myOled::begin( uint16_t I2C_speed , uint8_t I2c_address)
 {
 	_I2C_speed = I2C_speed;
 	_I2C_address = I2c_address;
-	OLED_I2C_ON();
-	OLEDinit();
-	OLED_I2C_OFF();
+	I2C_ON();
+	init();
+	I2C_OFF();
 }
 
-void myOled::OLED_I2C_ON()
+void myOled::I2C_ON()
 {
 	if (!bcm2835_i2c_begin())
 	{
@@ -39,12 +39,12 @@ void myOled::OLED_I2C_ON()
 	}
 }
 
-void myOled::OLED_I2C_OFF(void)
+void myOled::I2C_OFF(void)
 {
 	bcm2835_i2c_end();
 }
 
-void myOled::OLEDinit()
+void myOled::init()
  {
 
 	bcm2835_delay(myOled_INITDELAY);
@@ -97,6 +97,34 @@ switch (_OLED_HEIGHT)
 	bcm2835_delay(myOled_INITDELAY);
 }
 
+void myOled::drawPixel(int16_t x, int16_t y, uint8_t color)
+{
+
+	if ((x < 0) || (x >= this->bufferWidth) || (y < 0) || (y >= this->bufferHeight)) {
+	return;
+	}
+	int16_t temp;
+	switch (_rotation) {
+	case 1:
+		temp = x;
+		x = WIDTH - 1 - y;
+		y = temp;
+	break;
+	case 2:
+		x = WIDTH - 1 - x;
+		y = HEIGHT - 1 - y;
+	break;
+	case 3:
+		temp = x;
+		x = y;
+		y = HEIGHT - 1 - temp;
+	break;
+	}
+		uint16_t tc = (bufferWidth * (y /8)) + x;
+		this->buffer[tc] |= (1 << (y & 7));
+}
+
+
 void myOled::I2C_Write_Byte(uint8_t value, uint8_t cmd)
 {
 	char buf[2] = {cmd,value};
@@ -116,13 +144,13 @@ void myOled::I2C_Write_Byte(uint8_t value, uint8_t cmd)
 	}
 }
 
-void myOled::OLEDupdate()
+void myOled::update()
 {
 	uint8_t x = 0; uint8_t y = 0; uint8_t w = this->bufferWidth; uint8_t h = this->bufferHeight;
 	OLEDBuffer( x,  y,  w,  h, (uint8_t*) this->buffer);
 }
 
-void myOled::OLEDclearBuffer()
+void myOled::clearBuffer()
 {
 
 	memset( this->buffer, 0x00, (this->bufferWidth * (this->bufferHeight /8))  );
@@ -131,7 +159,7 @@ void myOled::OLEDclearBuffer()
 
 void myOled::OLEDBuffer(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t* data)
 {
-	OLED_I2C_ON();
+	I2C_ON();
 	uint8_t tx, ty;
 	uint16_t offset = 0;
 		
@@ -160,7 +188,73 @@ void myOled::OLEDBuffer(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t* dat
 			myOled_data(data[offset++]);
 		}
 	}
-	OLED_I2C_OFF();
+	I2C_OFF();
 
 }
 
+size_t myOled::print(const std::string &s) {
+    return write((const uint8_t *)s.c_str(), s.length());
+}
+
+size_t myOled::write(const uint8_t *buffer, size_t size)
+{
+    size_t n = 0;
+    while (size--) {
+    if (write(*buffer++)) n++;
+    else break;
+    }
+    return n;
+}
+
+size_t myOled::write(uint8_t character) 
+{
+    if (character == '\n') {
+    _cursor_y += _textsize*_CurrentFontheight;
+    _cursor_x  = 0;
+    } else if (character== '\r') {
+    // skip 
+    } else {
+    drawChar(_cursor_x, _cursor_y, character, _textcolor, _textbgcolor, _textsize);
+    _cursor_x += _textsize*(_CurrentFontWidth+1);
+        if (_textwrap && (_cursor_x > (_width - _textsize*(_CurrentFontWidth+1)))) {
+          _cursor_y += _textsize*_CurrentFontheight;
+          _cursor_x = 0;
+        }
+    }
+  return 1;
+}
+
+void myOled::setCursor(int16_t x, int16_t y) {
+	_cursor_x = x;
+	_cursor_y = y;
+}
+
+void myOled::drawChar(int16_t x, int16_t y, unsigned char c, uint8_t color, uint8_t bg, uint8_t size) {
+
+	if((x >= _width)            || // Clip right
+	 (y >= _height)           || // Clip bottom
+	 ((x + (_CurrentFontWidth+1) * size - 1) < 0) || // Clip left
+	 ((y + _CurrentFontheight * size - 1) < 0))   // Clip top
+	return;
+
+	for (int8_t i=0; i<(_CurrentFontWidth+1); i++ ) {
+	uint8_t line;
+	if (i == _CurrentFontWidth)
+	{ 
+		line = 0x0;
+	}
+	else 
+	{
+		line = pFontDefaultptr[((c - _CurrentFontoffset) * _CurrentFontWidth) + i];
+	}
+
+	for (int8_t j = 0; j<_CurrentFontheight ; j++) 
+	{
+		if (line & 0x1) 
+		{
+			drawPixel(x+i, y+j, color);
+		}
+		line >>= 1;
+	}
+	}
+}
